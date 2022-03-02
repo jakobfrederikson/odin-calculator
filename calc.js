4a73d99ea0a92ac03963e937cdede51de8128ebe
Jakob <jakobfred1999@gmail.com>
2022-03-02 17:16:46 +1300

    Create basic calculator outline

--- calc.js
+++ calc.js
@@ -13,3 +13,16 @@ let multiply = (a, b) => {
 let divide = (a, b) => {
     return a / b;
 }
+
+let operate = (operator, num1, num2) => {
+    switch (operator) {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+    }
+}
